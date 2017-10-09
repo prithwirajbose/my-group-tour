0c00a78c981fb15a6c64934764b46fd05335c270
Bose <prithwiraj.bose@experian.com>
2017-10-09 19:24:26 +0530

    webapp basic structure creation

---
+++ webapp/includes/head.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>My Group Tour</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/full.css" rel="stylesheet">
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
---
+++ webapp/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <title>My Group Tour</title>
+    <?php include_once("includes/head.php"); ?>
+  </head>
+
+  <body>
+
+   <?php include_once("includes/nav.php"); ?>
+
+    <!-- Page Content -->
+
+<div class="container centertext">
+      <h1 class="mt-5 appheading">My Group Tour</h1>
+      <p><button type="button" class="btn btn-success btn-lg btn-block">Login</button>
+<button type="button" class="btn btn-primary btn-lg btn-block">Create New Account</button></p>
+    </div>
+    
+    <?php include_once("includes/footer.php"); ?>
+
+  </body>
+
+</html>
